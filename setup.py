b5940f2735bad17613d5d5e88bb0d75d5c70a221
Yasin Boelhouwer <yasin@enginebit.com>
2024-11-09 13:41:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="realtimeregister",
-    version="1.0.0",
+    version="1.0.1",
     author="Makafeli",
     author_email="info@yasin.nu",
     description="Python SDK for Realtime Register API",
